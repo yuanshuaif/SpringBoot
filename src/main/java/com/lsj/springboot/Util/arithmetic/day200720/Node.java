a417771c61b597f2947abf7efa3243b35ea2b658
袁帅 <yuanshuaif@yonyou.com>
2020-07-23 18:56:41 +0800

    小程序(10)：将2个链表的数相加得到一个新链表

---
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Node.java
@@ -0,0 +1,34 @@
+package com.lsj.springboot.Util.arithmetic.day200720;
+
+public class Node {
+    int value;
+    Node next;
+    Node(int value){
+        this.value = value;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                ", next=" + next +
+                '}';
+    }
+}
