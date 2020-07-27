cf96c6d6daf5624e278d2fc8057f23015f529058
yuanshuaif <yuanshuaif@yonyou.com>
2020-07-27 23:03:15 +0800

    小程序(25)： 最大子序和

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200723/MiniProgram.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200723/MiniProgram.java
@@ -40,6 +40,9 @@ import java.util.Stack;
  * 入栈法
  *
  * 409. 最长回文串
+ *
+ * 53. 最大子序和
+ * 动态规划
  */
 public class MiniProgram {
 
@@ -464,6 +467,35 @@ public class MiniProgram {
         }
 
         return ans < s.length() ? (ans + 1) : ans;
+    }
+
+    /**
+     * 53. 最大子序和
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4]  输出: 6
+        解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+         动态规划的是首先对数组进行遍历，当前最大连续子序列和为 sum，结果为 ans
+         如果 sum > 0，则说明 sum 对结果有增益效果，则 sum 保留并加上当前遍历数字
+         如果 sum <= 0，则说明 sum 对结果无增益效果，需要舍弃，则 sum 直接更新为当前遍历数字
+         每次比较 sum 和 ans的大小，将最大值置为ans，遍历结束返回结果
+         时间复杂度：O(n)O(n)
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int hSum = 0;// 历史和
+        int sum = 0;// 当前和
+        int ans = nums[0];// 最大和
+        for(int num : nums){
+            if(hSum <= 0){
+                sum = num;
+            }else{
+                sum += num;
+            }
+            hSum = sum;
+            ans = Math.max(ans, sum);
+        }
+        return ans;
     }
 
 }
