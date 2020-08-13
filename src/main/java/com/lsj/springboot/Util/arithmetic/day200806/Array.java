15862e989c845d8ddb5241c2d75ddba9fa2726b5
袁帅 <yuanshuaif@yonyou.com>
2020-08-13 20:28:03 +0800

    小程序(51):验证回文串

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -31,6 +31,9 @@ import java.util.List;
  *
  * 题目283:移动零
  * 双指针
+ *
+ * 题目125:验证回文串
+ * 双指针
  */
 public class Array {
 
@@ -43,8 +46,9 @@ public class Array {
         System.out.println(A);*/
      /*   int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         threeSum(nums);*/
-        int[] nums = new int[]{0,1,0,3,12};
-        moveZeroes(nums);
+//        int[] nums = new int[]{0,1,0,3,12};
+//        moveZeroes(nums);
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
 
     }
 
@@ -422,7 +426,6 @@ public class Array {
      * @param nums
      */
     public static void moveZeroes(int[] nums) {
-
         int cur = 0;
         int len = 0;
         for(; len < nums.length; len++){
@@ -433,8 +436,39 @@ public class Array {
         for(int j = cur; j < len; j++){
             nums[j] = 0;
         }
-
         System.out.println(nums);
+    }
+
+    /**
+     * 125. 验证回文串
+     *  给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     * 输入: "A man, a plan, a canal: Panama" 输出: true
+     * 输入: "race a car" 输出: false
+     * @param s
+     * @return
+     */
+    public static boolean isPalindrome(String s) {
+        if(s == null || s.isEmpty()){
+            return true;
+        }
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end){
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }
+            while(start < end && !Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }
+            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+                return false;
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
 
     }
 
