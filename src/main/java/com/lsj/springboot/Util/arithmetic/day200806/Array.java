bb5bef376c31983bee9b14ff2f2ce41cfee92317
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-17 21:47:14 +0800

    小程序(62)：在排序数组中查找元素的第一个和最后一个位置

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -22,6 +22,8 @@ import java.util.List;
  * 剑指 Offer 53 - I. 在排序数组中查找数字 I
  *
  * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ *
+ * 题目34：在排序数组中查找元素的第一个和最后一个位置（Offer 53 - I）
  */
 public class Array {
 
@@ -354,6 +356,37 @@ public class Array {
             target = nums[nums.length - 1] + 1;
         }
         return target;
+    }
+
+    /**
+     * 34. 在排序数组中查找元素的第一个和最后一个位置
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     *  你的算法时间复杂度必须是 O(log n) 级别。
+     *  如果数组中不存在目标值，返回 [-1, -1]。
+     *  输入: nums = [5,7,7,8,8,10], target = 8   输出: [3,4]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int[] ints = new int[]{-1, -1};
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                if(ints[0] == -1){
+                    ints[0] = i;
+                }
+                count++;
+            }else{
+                if(count > 0){
+                    break;
+                }
+            }
+        }
+        if(count > 0){
+            ints[1] = ints[0] + count - 1;
+        }
+        return ints;
     }
 
 
