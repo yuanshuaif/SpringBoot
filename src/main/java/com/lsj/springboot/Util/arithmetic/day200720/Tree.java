8019b2cee8bb0383f9338cbe86d76ca7357fa555
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-18 00:02:37 +0800

    小程序(64)：平衡二叉树

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
@@ -10,6 +10,8 @@ package com.lsj.springboot.Util.arithmetic.day200720;
  * 双递归
  *
  * 543. 二叉树的直径
+ *
+ * 110. 平衡二叉树
  */
 public class Tree {
 
@@ -92,6 +94,21 @@ public class Tree {
         int right = diameter(root.right);
         ans = Math.max(ans, left + right);
         return 1 + Math.max(left, right);
+    }
+
+    /**
+     * 110. 平衡二叉树
+     * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+     *本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1
+                && isBalanced(root.left) && isBalanced(root.right);
     }
 
 }
