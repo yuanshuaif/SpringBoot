9a87d4c594fa88da7ca3d57284b1503159775183
袁帅 <yuanshuaif@yonyou.com>
2020-08-18 11:38:13 +0800

    小程序(67):判断一个数是否是质数

---
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200818/Maths.java
@@ -0,0 +1,25 @@
+package com.lsj.springboot.Util.arithmetic.day200818;
+
+public class Maths {
+
+    // 判断一个数是否是质数
+    public static boolean isPrime(int src) {
+        double sqrt = Math.sqrt(src);
+        if (src < 2) {
+            return false;
+        }
+        if (src == 2 || src == 3) {
+            return true;
+        }
+        if (src % 2 == 0) {// 先判断是否为偶数，若偶数就直接结束程序
+            return false;
+        }
+        for (int i = 3; i <= sqrt; i += 2) {
+            if (src % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
