a2805c78dbc7df050b3fb1b3d8fb3fedb0410fcd
袁帅 <yuanshuaif@yonyou.com>
2020-08-19 12:50:34 +0800

    小程序(70): 存在重复元素 II

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Hash.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Hash.java
@@ -22,6 +22,8 @@ import java.util.*;
  * set也是hash表的一种
  *
  * 题目204:计数质数
+ *
+ * 题目219:存在重复元素 II
  */
 public class Hash {
 
@@ -213,6 +215,35 @@ public class Hash {
             }
         }
         return true;
+    }
+
+
+    /**
+     * 219. 存在重复元素 II
+     * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+     * 输入: nums = [1,2,3,1], k = 3  输出: true
+     * 输入: nums = [1,0,1,1], k = 1  输出: true
+     * 输入: nums = [1,2,3,1,2,3], k = 2  输出: false
+     * @param nums
+     * @param k
+     * @return
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+//        维护一个哈希表，里面始终最多包含 k 个元素，当出现重复值时则说明在 k 距离内存在重复元素
+//        每次遍历一个元素则将其加入哈希表中，如果哈希表的大小大于 k，则移除最前面的数字
+//        时间复杂度：O(n)，n 为数组长度
+
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i - k]);// 移除最前面的数字
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args){
