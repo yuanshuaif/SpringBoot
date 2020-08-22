96d82da2703f5382faa200160b53dad48cc7d2c8
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-22 10:20:48 +0800

    小程序(79)： 两整数之和 小程序(80)： 汉明距离 小程序(81)：多数元素

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200818/Maths.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200818/Maths.java
@@ -1,5 +1,12 @@
 package com.lsj.springboot.Util.arithmetic.day200818;
 
+import java.util.Arrays;
+
+/**
+ * 判断一个数是否是质数
+ *
+ *  169. 多数元素
+ */
 public class Maths {
 
     // 判断一个数是否是质数
@@ -20,6 +27,19 @@ public class Maths {
             }
         }
         return true;
+    }
+
+    /**
+     * 169. 多数元素
+     * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     *  你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+     * 输入: [2,2,1,1,1,2,2]  输出: 2
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 
 }
