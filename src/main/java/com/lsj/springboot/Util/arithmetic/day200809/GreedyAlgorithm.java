aaa1f0d025efbd13f25406ec196fd0b7ed3b779c
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-30 15:40:03 +0800

    小程序(106)：分发饼干

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200809/GreedyAlgorithm.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200809/GreedyAlgorithm.java
@@ -1,5 +1,7 @@
 package com.lsj.springboot.Util.arithmetic.day200809;
 
+import java.util.Arrays;
+
 /**
  * Created by 10326 on 2020/8/30.
  * 贪心算法
@@ -13,6 +15,8 @@ package com.lsj.springboot.Util.arithmetic.day200809;
  * 题目860:柠檬水找零
  *
  * 题目605:种花问题
+ *
+ * 题目455:分发饼干
  */
 public class GreedyAlgorithm {
 
@@ -82,6 +86,33 @@ public class GreedyAlgorithm {
             }
         }
         return count >= n;
+    }
+
+    /**
+     * 455. 分发饼干
+     * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。对每个孩子 i ，都有一个胃口值 gi ，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j ，都有一个尺寸 sj 。
+     * 如果 sj >= gi ，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+     * 你可以假设胃口值为正。一个小朋友最多只能拥有一块饼干。
+     * 输入: [1,2,3], [1,1]    输出: 1
+     * 解释:你有三个孩子和两块小饼干，3个孩子的胃口值分别是：1,2,3。 虽然你有两块小饼干，由于他们的尺寸都是1，你只能让胃口值是1的孩子满足。所以你应该输出1。
+     * @param g
+     * @param s
+     * @return
+     */
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int cur = 0;
+        int ans = 0;
+        for(int i = 0; i < g.length; i++){
+            while(cur < s.length){
+                if(g[i] <= s[cur++]){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 
 }
