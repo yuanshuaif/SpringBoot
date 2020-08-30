7f866785ee31806addd0bce8259eaa85aed1ea54
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-30 15:03:01 +0800

    小程序(105)：柠檬水找零

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -17,8 +17,6 @@ import java.util.List;
  *
  * 题目21:合并两个有序链表（数组）
  *
- * 题目605:种花问题
- *
  * 剑指 Offer 53 - I. 在排序数组中查找数字 I
  *
  * 剑指 Offer 53 - II. 0～n-1中缺失的数字
@@ -283,35 +281,6 @@ public class Array {
             }
         }
         return combine.toArray(new Integer[combine.size()]);
-    }
-
-    /**
-     * 605. 种花问题
-     * 假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。
-     * 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。
-     *  输入: flowerbed = [1,0,0,0,1], n = 1 输出: True;                输入: flowerbed = [1,0,0,0,1], n = 2 输出: False
-     *  注意:1.数组内已种好的花不会违反种植规则。2.输入的数组长度范围为 [1, 20000]。3. n 是非负整数，且不会超过输入数组的大小。
-     * @param flowerbed
-     * @param n
-     * @return
-     */
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        if(flowerbed == null){
-            return false;
-        }
-        int count = 0;
-        for(int i = 0; i < flowerbed.length; i++){
-            //贪心算法
-            // 当前节点如果是头结点：当前节点是0，下一个节点是0，可种植数+1，当前节点由0变1；
-            // 当前节点如果是尾结点：当前节点是0，前一个节点是0，可种植数+1，当前节点由0变1；
-            // 中间节点：当前节点是0，前一个节点是0，后一个节点是0，可种植数+1，当前节点由0变1；
-            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
-                    (i == flowerbed.length - 1  || flowerbed[i + 1] == 0)){
-                count ++;
-                flowerbed[i] = 1;
-            }
-        }
-        return count >= n;
     }
 
     /**
