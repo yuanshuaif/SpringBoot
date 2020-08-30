c9cea4c55da8635c2aea9b08e1b1e6eae3a4109a
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-30 20:41:04 +0800

    小程序(109)：颜色分类

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Sort.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Sort.java
@@ -17,6 +17,8 @@ import java.util.Arrays;
  *
  *  面试题 17.14. 最小K个数
  *
+ *  75. 颜色分类
+ *
  */
 public class Sort {
     /**
@@ -230,6 +232,37 @@ public class Sort {
         if(right > fRight){
             sort(arr, fRight + 1, right);
         }
+    }
+
+    /**
+     * 75. 颜色分类
+     * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+     * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+     * 输入: [2,0,2,1,1,0]    输出: [0,0,1,1,2,2]
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        // 三色问题
+        int start = 0;
+        int end = nums.length - 1;
+        int cur = 0;
+        while(cur <= end){
+            if(nums[cur] == 0){
+                swap(nums, start, cur);
+                cur++;
+                start++;
+            }else if(nums[cur] == 2){
+                swap(nums, cur, end);
+                end--;
+            }else{
+                cur++;
+            }
+        }
+    }
+    public void swap(int[] nums, int start, int end){
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
     }
 
     public static void main(String[] args){
