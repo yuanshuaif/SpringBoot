67d5bd65c3a75e0ecb7c69f59fe852512748f7ef
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-31 21:58:40 +0800

    小程序(117)：删除排序数组中的重复项 II

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/DoublePointer.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/DoublePointer.java
@@ -37,6 +37,8 @@ import java.util.*;
  *
  * 题目925:长按键入
  *
+ * 题目80:删除排序数组中的重复项 II
+ *
  */
 public class DoublePointer {
 
@@ -470,5 +472,28 @@ public class DoublePointer {
         }
 
         return true;
+    }
+
+    /**
+     * 80. 删除排序数组中的重复项 II
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * 给定 nums = [1,1,1,2,2,3],函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3 。你不需要考虑数组中超出新长度后面的元素。
+     * 给定 nums = [0,0,1,1,1,1,2,3,3],函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 2){
+            return nums.length;
+        }
+        int cur = 2;
+        for(int i = 2; i < nums.length; i++){
+            if(nums[i] == nums[cur - 1] && nums[i] == nums[cur - 2]){
+            }else{
+                nums[cur++] = nums[i];
+            }
+        }
+        return cur;
     }
 }
