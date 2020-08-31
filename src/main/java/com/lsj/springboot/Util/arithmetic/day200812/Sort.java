87a429531ab50dfcc4f7309ccfbd13476668a0b1
yuanshuaif <yuanshuaif@yonyou.com>
2020-08-31 21:20:50 +0800

    小程序(115)：你可以获得的最大硬币数目 小程序(116)：最大数

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Sort.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Sort.java
@@ -1,6 +1,7 @@
 package com.lsj.springboot.Util.arithmetic.day200812;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  *  1491. 去掉最低工资和最高工资后的工资平均值
@@ -24,6 +25,10 @@ import java.util.Arrays;
  *  1502. 判断能否形成等差数列
  *
  *  1122. 数组的相对排序
+ *
+ *  1561. 你可以获得的最大硬币数目
+ *
+ *  179. 最大数
  *
  */
 public class Sort {
@@ -350,6 +355,57 @@ public class Sort {
             arr1[cur++] = temp[i];
         }
         return arr1;
+    }
+
+    /**
+     * 1561. 你可以获得的最大硬币数目
+     * 有 3n 堆数目不一的硬币，你和你的朋友们打算按以下方式分硬币：
+     * 每一轮中，你将会选出 任意 3 堆硬币（不一定连续）。Alice 将会取走硬币数量最多的那一堆。你将会取走硬币数量第二多的那一堆。
+     *  Bob 将会取走最后一堆。重复这个过程，直到没有更多硬币。
+     *  给你一个整数数组 piles ，其中 piles[i] 是第 i 堆中硬币的数目。
+     * 输入：piles = [2,4,1,2,7,8] 输出：9
+     * 解释：选出 (2, 7, 8) ，Alice 取走 8 枚硬币的那堆，你取走 7 枚硬币的那堆，Bob 取走最后一堆。
+     *      选出 (1, 2, 4) , Alice 取走 4 枚硬币的那堆，你取走 2 枚硬币的那堆，Bob 取走最后一堆。
+     *      你可以获得的最大硬币数目：7 + 2 = 9.
+     * @param piles
+     * @return
+     */
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int ans = 0;
+        for(int i = 0; i < piles.length / 3; i++){
+            ans += piles[piles.length - 2 * (i + 1)];
+        }
+        return ans;
+    }
+
+    /**
+     * 179. 最大数
+     * 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
+     * 输入: [10,2]      输出: 210          输入: [3,30,34,5,9]     输出: 9534330
+     * [0,00] 0]
+     * @param nums
+     * @return
+     */
+    public String largestNumber(int[] nums) {
+        Integer[] temp = new Integer[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            temp[i] = nums[i];
+        }
+        Arrays.sort(temp, new Comparator<Integer>(){
+            @Override
+            public int compare(Integer a, Integer b){
+                String s1 = a + "" + b;// 102
+                String s2 = b + "" + a;// 210
+                return s2.compareTo(s1);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int num : temp){
+            sb.append(num);
+        }
+        //[0,00]
+        return sb.toString().charAt(0) == '0' ? "0" : sb.toString();
     }
 
     public static void main(String[] args){
