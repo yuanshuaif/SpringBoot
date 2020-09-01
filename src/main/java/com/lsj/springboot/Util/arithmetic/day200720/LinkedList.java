04aa095d660d36f3939dc989147191edfcab616c
袁帅 <yuanshuaif@yonyou.com>
2020-09-01 16:03:50 +0800

    小程序(118): 环形链表 II

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200720/LinkedList.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/LinkedList.java
@@ -51,6 +51,8 @@ import java.util.*;
  *
  * 19. 删除链表的倒数第N个节点
  *
+ * 142. 环形链表 II
+ *
  */
 public class LinkedList {
 
@@ -732,6 +734,47 @@ public class LinkedList {
         
         return head;
 
+    }
+
+    /**
+     * 142. 环形链表 II
+     * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+     * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+     * 说明：不允许修改给定的链表。
+     * 输入：head = [3,2,0,-4], pos = 1    输出：tail connects to node index 1    解释：链表中有一个环，其尾部连接到第二个节点。
+     * 输入：head = [1,2], pos = 0         输出：tail connects to node index 0    解释：链表中有一个环，其尾部连接到第一个节点。
+     * 输入：head = [1], pos = -1          输出：no cycle                         解释：链表中没有环。
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        // 快慢指针
+        // a(开始位置->入环口) b（入环口走多少步相遇） c（再走多少步到达入环口） l（环长）
+        // b + c = kl; 2(b + a) = a + b + kl   ==> a = c
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode first = head.next;
+        ListNode second = head.next.next;
+        while(first != null && second != null){
+            if(first == second){
+                break;
+            }
+            first = first.next;
+            if(second.next == null){
+                return null;
+            }
+            second = second.next.next;
+        }
+        first = head;
+        while(first != second){
+            first = first.next;
+            if(second == null) {
+                return null;
+            }
+            second = second.next;
+        }
+        return first;
     }
 
     public static void main(String[] args){
