4e89a6b10f5150caea984944cda4eabef199dc48
袁帅 <yuanshuaif@yonyou.com>
2020-09-01 16:28:45 +0800

    小程序(119): 寻找重复数

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/DoublePointer.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/DoublePointer.java
@@ -39,6 +39,8 @@ import java.util.*;
  *
  * 题目80:删除排序数组中的重复项 II
  *
+ * 题目287:寻找重复数
+ *
  */
 public class DoublePointer {
 
@@ -495,5 +497,46 @@ public class DoublePointer {
             }
         }
         return cur;
+    }
+
+    /**
+     * 287. 寻找重复数
+     * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+     * 假设只有一个重复的整数，找出这个重复的数。
+     * 输入: [1,3,4,2,2]——输出: 2              输入: [3,1,3,4,2]——输出: 3
+     * 1.不能更改原数组（假设数组是只读的）。(不能用排序)      2.只能使用额外的 O(1) 的空间。（不能用hash表）
+     * 3.时间复杂度小于 O(n2) 。                4.数组中只有一个重复的数字，但它可能不止重复出现一次。
+     * @param nums
+     * @return
+     */
+    public int findDuplicate(int[] nums) {
+        // 不存在[1],[1,2],[1,2,3]这种情况
+     /*   if(nums.length == 0){
+            return -1;
+        }
+        int cur = 0;
+        for(int i = 1; i < nums.length; i++){
+            for(int j = 0; j <= cur; j++){
+                if(nums[i] == nums[j]){
+                    return nums[i];
+                }
+            }
+            cur++;
+        }
+        return 0;*/
+        int slow = 0, fast = 0;
+        do {
+            // 3 4 -> 4 3 -> 2 2    // 0 2    // 3 3
+            // a(开始位置->入环口) b（入环口走多少步相遇） c（再走多少步到达入环口） l（环长）
+            // b + c = kl; 2(b + a) = a + b + kl   ==> a = c
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 }
