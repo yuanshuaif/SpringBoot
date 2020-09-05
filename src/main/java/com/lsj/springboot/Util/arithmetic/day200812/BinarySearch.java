032e900bba8a4b17e9d4a471f08cf34c44a2f137
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-05 17:59:03 +0800

    小程序(134):搜索二维矩阵

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
@@ -22,6 +22,8 @@ import java.util.*;
  * 852. 山脉数组的峰顶索引
  *
  * 面试题 08.03. 魔术索引
+ *
+ * 74. 搜索二维矩阵
  */
 public class BinarySearch {
 
@@ -384,6 +386,53 @@ public class BinarySearch {
             return mid;
         }
         return findMagicIndex(nums, mid + 1, end);
+    }
+
+    /**
+     * 74. 搜索二维矩阵
+     * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+     * 每行中的整数从左到右按升序排列。每行的第一个整数大于前一行的最后一个整数。
+     * 输入:matrix = [[1,   3,  5,  7],[10, 11, 16, 20],[23, 30, 34, 50]] target = 3  输出: true
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix0(int[][] matrix, int target) {
+        if(matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int len = matrix[0].length;
+        int first = 0;
+        int last = matrix.length - 1;
+        int cur = -1;
+        while(first <= last){
+            int mid = (first + last) / 2;
+            if(matrix[mid][0] > target){
+                last = mid - 1;
+            }else if(matrix[mid][len - 1] < target){
+                first = mid + 1;
+            }else{
+                cur = mid;
+                break;
+            }
+        }
+        if(cur == -1){
+            return false;
+        }else{
+            int start = 0;
+            int end = matrix[0].length - 1;
+            while(start <= end){
+                int mid = (start + end) / 2;
+                if(matrix[cur][mid] > target){
+                    end = mid - 1;
+                }else if(matrix[cur][mid] < target){
+                    start = mid + 1;
+                }else{
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     public static void main(String[] args){
