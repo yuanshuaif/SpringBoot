8a5f301a7ef7046cc51491253e61299aeb249a83
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-05 18:30:08 +0800

    小程序(135):寻找旋转排序数组中的最小值

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
@@ -24,6 +24,8 @@ import java.util.*;
  * 面试题 08.03. 魔术索引
  *
  * 74. 搜索二维矩阵
+ *
+ * 153. 寻找旋转排序数组中的最小值
  */
 public class BinarySearch {
 
@@ -433,6 +435,32 @@ public class BinarySearch {
             }
             return false;
         }
+    }
+
+    /**
+     * 153. 寻找旋转排序数组中的最小值
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+     * 请找出其中最小的元素。你可以假设数组中不存在重复元素。
+     * 输入: [3,4,5,1,2]  输出: 1
+     * 输入: [4,5,6,7,0,1,2]  输出: 0
+     * @param nums
+     * @return
+     */
+    public int findMin(int[] nums) {
+        int mid;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            mid = (start + end) / 2;
+            if(nums[mid] >= nums[0]){// = [2,1]
+                start = mid + 1;
+            }else if(nums[mid] > nums[mid - 1]){
+                end = mid - 1;
+            }else{
+                return nums[mid];
+            }
+        }
+        return nums[0];
     }
 
     public static void main(String[] args){
