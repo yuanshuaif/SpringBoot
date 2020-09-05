1717ca332640997575e288a5de663e5021758cf8
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-05 23:00:50 +0800

    小程序(136):统计有序矩阵中的负数

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
@@ -26,6 +26,8 @@ import java.util.*;
  * 74. 搜索二维矩阵
  *
  * 153. 寻找旋转排序数组中的最小值
+ *
+ * 1351. 统计有序矩阵中的负数
  */
 public class BinarySearch {
 
@@ -461,6 +463,36 @@ public class BinarySearch {
             }
         }
         return nums[0];
+    }
+
+    /**
+     * 1351. 统计有序矩阵中的负数
+     * 给你一个 m * n 的矩阵 grid，矩阵中的元素无论是按行还是按列，都以非递增顺序排列。请你统计并返回 grid 中 负数 的数目。
+     * 输入：grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]  输出：8   解释：矩阵中共有 8 个负数。
+     * 输入：grid = [[3,2],[1,0]]  输出：0        输入：grid = [[1,-1],[-1,-1]]   输出：3
+     * [[3,2],[-3,-3],[-3,-3],[-3,-3]]
+     * @param grid
+     * @return
+     */
+    public int countNegatives(int[][] grid) {
+        if(grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int row = 0;
+        int col = grid[0].length - 1;
+        int len = 0;
+        int ans = 0;
+        while(col >= 0 && row <= grid.length - 1){
+            if(grid[row][col] < 0){
+                col--;
+                len++;
+            }else{
+                ans += len * (grid.length - row);
+                row++;
+                len = 0;
+            }
+        }
+        return len > 0 ? (len * (grid.length - row)  + ans) : ans;
     }
 
     public static void main(String[] args){
