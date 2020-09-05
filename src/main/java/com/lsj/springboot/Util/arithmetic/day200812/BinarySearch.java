3684dfcb8bf825a5ca8cd18b3d23af4d6a696cca
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-05 17:32:49 +0800

    小程序(133):魔术索引

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/BinarySearch.java
@@ -20,6 +20,8 @@ import java.util.*;
  * 面试题 10.05. 稀疏数组搜索
  *
  * 852. 山脉数组的峰顶索引
+ *
+ * 面试题 08.03. 魔术索引
  */
 public class BinarySearch {
 
@@ -357,7 +359,35 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 面试题 08.03. 魔术索引
+     * 魔术索引。 在数组A[0...n-1]中，有所谓的魔术索引，满足条件A[i] = i。
+     * 给定一个有序整数数组，编写一种方法找出魔术索引，若有的话，在数组A中找出一个魔术索引，如果没有，则返回-1。若有多个魔术索引，返回索引值最小的一个。
+     *  输入：nums = [0, 2, 3, 4, 5]   输出：0    说明: 0下标的元素为0
+     *  输入：nums = [0, 0，2]   输出：0
+     * @param nums
+     * @return
+     */
+    public static int findMagicIndex(int[] nums) {
+        return findMagicIndex(nums, 0, nums.length - 1);
+    }
+
+    public static int findMagicIndex(int[] nums, int start, int end) {
+        if(start > end){
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        int left = findMagicIndex(nums, start, mid - 1);
+        if(left != -1){
+            return left;
+        }else if(mid == nums[mid]){
+            return mid;
+        }
+        return findMagicIndex(nums, mid + 1, end);
+    }
+
     public static void main(String[] args){
-        System.out.println(isMajorityElement(new int[]{1,2,3,4,5}, 3));
+//        System.out.println(isMajorityElement(new int[]{1,2,3,4,5}, 3));
+        System.out.println(findMagicIndex(new int[]{0,0,2}));
     }
 }
