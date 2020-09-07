ff902373267405f837fed3ca0fa86a5ea15ff9c1
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-07 22:20:04 +0800

    小程序(144):岛屿数量

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Hash.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/Hash.java
@@ -42,7 +42,6 @@ import java.util.*;
  *
  * 剑指 Offer 50. 第一个只出现一次的字符
  *
- * 题目463：岛屿的周长
  */
 public class Hash {
 
@@ -518,44 +517,6 @@ public class Hash {
             }
         }
         return ' ';
-    }
-
-    /**
-     * 463. 岛屿的周长
-     * 网络的深度优先搜索问题
-     * 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
-     * 网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
-     * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。
-     * 格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
-     * 输入:[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]输出: 16
-     * @param grid
-     * @return
-     */
-    public int islandPerimeter(int[][] grid) {
-        for(int r = 0; r < grid.length; r++){
-            for(int c = 0; c < grid[0].length; c++){
-                if(grid[r][c] == 1){
-                    return islandPerimeter(grid, r, c);
-                }
-            }
-        }
-        return 0;
-    }
-
-    public int islandPerimeter(int[][] grid, int r, int c){
-        if(!(r >= 0 && r < grid.length && c >= 0 && c < grid[0].length)){
-            return 1;// 从岛屿移动到边界外 +1
-        }
-        if(grid[r][c] == 0){
-            return 1;// 从岛屿移动到水域 +1
-        }
-        if(grid[r][c] == 2){// 已经算过的节点不在参与运算
-            return 0;
-        }
-        grid[r][c] = 2;
-        // 前后左右4个节点
-        return islandPerimeter(grid, r - 1, c) + islandPerimeter(grid, r + 1, c) +
-                islandPerimeter(grid, r, c - 1) + islandPerimeter(grid, r, c + 1);
     }
 
     public static void main(String[] args){
