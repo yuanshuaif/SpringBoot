d420cf403dcdd4b5b582e9c4d149c7eb911926e0
袁帅 <yuanshuaif@yonyou.com>
2020-09-08 22:01:52 +0800

    小程序(148):翻转字符串里的单词

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
@@ -18,6 +18,10 @@ import java.util.Set;
  *
  * 434. 字符串中的单词数
  *
+ * 151. 翻转字符串里的单词
+ *
+ * 剑指 Offer 58 - I. 翻转单词顺序
+ *
  * Created by 10326 on 2020/8/26.
  */
 public class Strings2 {
@@ -250,12 +254,56 @@ public class Strings2 {
         return ans;
     }
 
+    /**
+     * 151. 翻转字符串里的单词
+     * 剑指 Offer 58 - I. 翻转单词顺序
+     * 给定一个字符串，逐个翻转字符串中的每个单词。
+     * 输入: "the sky is blue"    输出: "blue is sky the"
+     * 输入: "  hello world!  "   输出: "world! hello"      解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * 输入: "a good   example"   输出: "example good a"    解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     *
+     * 1.无空格字符构成一个单词。
+     * 2.输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * 3.如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     * @param s
+     * @return
+     */
+    public static String reverseWords(String s) {
+        s = s.trim();
+        if("".equals(s)){
+            return "";
+        }
+        int start = 0;
+        int end = 0;
+        String ans = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(' ' != s.charAt(i)){
+                end++;
+            }else{
+                if(start != end) {
+                    sb.insert(0, " " + s.substring(start, end));
+                }
+                end++;
+                start = end;
+
+            }
+        }
+        if(start != end) {
+            sb.insert(0, " " + s.substring(start, end));
+        }
+        ans = sb.substring(1);
+        return ans;
+    }
+
     public static void main(String[] args){
 //        System.out.println(validPalindrome("lcuppucul") );
 //        System.out.println(maxPower("t") );
 //        System.out.println(CheckPermutation("abc", "bca") );
 //        System.out.println(reverseOnlyLetters("Test1ng-Leet=code-Q!") );
-        System.out.println(countSegments(", , , ,        a, eaefa") );
+//        System.out.println(countSegments(", , , ,        a, eaefa") );
+
+        System.out.println(reverseWords("the sky is blue") );
 
     }
 }
