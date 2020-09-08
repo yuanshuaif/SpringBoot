5cd87fe02f5830e073f9d65abf49815180f45f94
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-09 00:43:52 +0800

    小程序(149):机器人能否返回原点

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
@@ -22,6 +22,8 @@ import java.util.Set;
  *
  * 剑指 Offer 58 - I. 翻转单词顺序
  *
+ * 657. 机器人能否返回原点
+ *
  * Created by 10326 on 2020/8/26.
  */
 public class Strings2 {
@@ -294,6 +296,34 @@ public class Strings2 {
         }
         ans = sb.substring(1);
         return ans;
+    }
+
+    /**
+     * 657. 机器人能否返回原点
+     * 在二维平面上，有一个机器人从原点 (0, 0) 开始。给出它的移动顺序，判断这个机器人在完成移动后是否在 (0, 0) 处结束。
+     * 移动顺序由字符串表示。字符 move[i] 表示其第 i 次移动。机器人的有效动作有 R（右），L（左），U（上）和 D（下）。如果机器人在完成所有动作后返回原点，则返回 true。否则，返回 false。
+     * 注意：机器人“面朝”的方向无关紧要。 “R” 将始终使机器人向右移动一次，“L” 将始终向左移动等。此外，假设每次移动机器人的移动幅度相同。
+     * 输入: "UD" 输出: true    解释：机器人向上移动一次，然后向下移动一次。所有动作都具有相同的幅度，因此它最终回到它开始的原点。因此，我们返回 true。
+     * @param moves
+     * @return
+     */
+    public boolean judgeCircle(String moves) {
+        // 定义坐标原点
+        int x = 0;
+        int y = 0;
+        char[] chars = moves.toCharArray();
+        for(char ch : chars){
+            if(ch == 'R'){
+                y++;
+            }else if(ch == 'L'){
+                y--;
+            }else if(ch == 'U'){
+                x++;
+            }else if(ch == 'D'){
+                x--;
+            }
+        }
+        return x == 0 && y == 0;
     }
 
     public static void main(String[] args){
