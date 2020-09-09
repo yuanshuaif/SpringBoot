d3be54d6804daea2ebd2ebf6ba56f5867f35aebc
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-10 00:00:59 +0800

    小程序(152): 组合总和

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200818/Backtracking.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200818/Backtracking.java
@@ -7,6 +7,8 @@ import java.util.List;
 /**
  * 回溯算法
  * 78. 子集
+ *
+ * 39. 组合总和
  */
 public class Backtracking {
 
@@ -83,6 +85,44 @@ public class Backtracking {
             temp.add(nums[j]);// 本层做出选择
             subsets2(j + 1, ans, nums, temp);// 进入下一层做出选择
             temp.remove(temp.size() - 1);//本层做出调整
+        }
+    }
+
+    /**
+     * 39. 组合总和
+     * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+     * candidates 中的数字可以无限制重复被选取。
+     * 所有数字（包括 target）都是正整数。 解集不能包含重复的组合。
+     * 输入：candidates = [2,3,6,7], target = 7,所求解集为：[[7],[2,2,3]]
+     * 输入：candidates = [2,3,5], target = 8,所求解集为：[[2,2,2,2],[2,3,3],[3,5]]
+     * 1 <= candidates.length <= 30         1 <= candidates[i] <= 200       candidate 中的每个元素都是独一无二的。        1 <= target <= 500
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if(candidates.length == 0){
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        combinationSum(candidates, target, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    public void combinationSum(int[] candidates, int target, int start,
+                               List<List<Integer>> res, List<Integer> temp) {
+        if(target == 0){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            if(target - candidates[i] < 0){
+                return;
+            }
+            temp.add(candidates[i]);
+            combinationSum(candidates, target - candidates[i], i, res, temp);
+            temp.remove(temp.size() - 1);
         }
     }
 
