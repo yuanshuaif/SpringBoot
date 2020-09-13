ac8cde39ddaf698d005d37b38b7171a96824c28f
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-13 11:00:35 +0800

    小程序(159):将有序数组转换为二叉搜索树

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
@@ -27,6 +27,8 @@ import java.util.LinkedList;
  * 101. 对称二叉树
  *
  * 637. 二叉树的层平均值
+ *
+ * 108. 将有序数组转换为二叉搜索树
  */
 public class Tree {
 
@@ -265,6 +267,30 @@ public class Tree {
             ans.add(sum / len);
         }
         return ans;
+    }
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+     * 给定有序数组: [-10,-3,0,5,9],一个可能的答案是：[0,-3,9,-10,null,5,null]，它可以表示下面这个高度平衡二叉搜索树：
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        // 以中间节点或者中间节点的左节点作为根节点
+        return sortedArrayToBST(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int start, int end) {
+        if(start > end){
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBST(nums, start, mid - 1);
+        node.right = sortedArrayToBST(nums, mid + 1, end);
+        return node;
     }
 
 }
