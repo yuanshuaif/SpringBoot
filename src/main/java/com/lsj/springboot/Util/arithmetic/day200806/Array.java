61c44fa22d31f3f1a284279a9ef65132d3dad405
袁帅 <yuanshuaif@yonyou.com>
2020-09-17 20:08:30 +0800

    小程序(176):数组拆分 I

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -25,11 +25,13 @@ import java.util.*;
  *
  * 题目1099：小于 K 的两数之和
  *
- * 905. 按奇偶排序数组
+ * 905：按奇偶排序数组
  *
  * 1287. 有序数组中出现次数超过25%的元素
  *
  * 1528. 重新排列字符串
+ *
+ * 561. 数组拆分 I
  */
 public class Array {
 
@@ -512,6 +514,23 @@ public class Array {
             result[indices[i]] = s.charAt(i);
         }
         return String.valueOf(result);
+    }
+
+    /**
+     * 561. 数组拆分 I
+     * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+     * 输入: [1,4,3,2]    输出: 4    解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+     * 1.n 是正整数,范围在 [1, 10000].     2.数组中的元素范围在 [-10000, 10000].
+     * @param nums
+     * @return
+     */
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i = 0; i < nums.length; i = i + 2){
+            ans += nums[i];
+        }
+        return ans;
     }
 
     public static void main(String[] args){
