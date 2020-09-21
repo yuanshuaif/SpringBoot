e9e8900e9dba1e30beadb5a834553ed9e9d27ab0
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-21 23:18:21 +0800

    小程序(184): 把二叉搜索树转换为累加树

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
@@ -62,6 +62,10 @@ import java.util.LinkedList;
  * 199. 二叉树的右视图
  *
  * 515. 在每个树行中找最大值
+ *
+ * 538. 把二叉搜索树转换为累加树
+ *
+ * 1038. 从二叉搜索树到更大和树
  */
 public class Tree {
 
@@ -817,6 +821,32 @@ public class Tree {
             ans.add(max);
         }
         return ans;
+    }
+
+    /**
+     * 538. 把二叉搜索树转换为累加树
+     * 1038. 从二叉搜索树到更大和树
+     * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+     * 输入: 原始二叉搜索树:
+         5
+      /   \
+     2     13
+     输出: 转换为累加树:
+          18
+        /   \
+     20     13
+     */
+    // 逆序中序遍历
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
     }
 
     public static void main(String[] args){
