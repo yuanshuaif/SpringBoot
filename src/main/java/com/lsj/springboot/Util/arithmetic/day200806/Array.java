7081fd925866f5d2339bf0dcfc0c4406920fa3b7
袁帅 <yuanshuaif@yonyou.com>
2020-09-25 12:14:00 +0800

    小程序(191):旋转图像

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -36,6 +36,8 @@ import java.util.*;
  * 题目941:有效的山脉数组
  *
  * 题目118:杨辉三角
+ *
+ * 题目48:旋转图像
  */
 public class Array {
 
@@ -598,6 +600,44 @@ public class Array {
             res.add(temp);
         }
         return res;
+    }
+
+    /**
+     * 48. 旋转图像
+     * 给定一个 n × n 的二维矩阵表示一个图像。将图像顺时针旋转 90 度。
+     * 说明：你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+     * 给定 matrix =
+     * [
+     *   [1,2,3],
+     *   [4,5,6],
+     *   [7,8,9]
+     * ],
+     * 原地旋转输入矩阵，使其变为:
+     * [
+     *   [7,4,1],
+     *   [8,5,2],
+     *   [9,6,3]
+     * ]
+     * 算法核心：先转置矩阵，然后翻转每一行
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int temp = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n / 2; j++){
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
     }
 
     public static void main(String[] args){
