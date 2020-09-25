a175271779c6a0974221f2385f02645a02e093be
袁帅 <yuanshuaif@yonyou.com>
2020-09-25 17:33:32 +0800

    暂存

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -640,6 +640,22 @@ public class Array {
         }
     }
 
+    /**
+     * 442. 数组中重复的数据
+     * 给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。找到所有出现两次的元素。
+     * 你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+     * 输入:[4,3,2,7,8,2,3,1]            输出:[2,3]
+     * 核心算法：
+     *      找到数字i时，将位置i-1处的数字翻转为负数。
+     *      如果位置i-1 上的数字已经为负，则i是出现两次的数字。
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDuplicates(int[] nums) {
+
+        return null;
+    }
+
     public static void main(String[] args){
       /*  int[] A = {1,2,3,0,0,0};
         int m = 3;
