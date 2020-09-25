2b28375cb38fbcbf8427a575a7928488c5a172f0
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-25 23:30:16 +0800

    小程序(194): 不同路径

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
@@ -34,6 +34,8 @@ import java.util.stream.Collectors;
  *
  *  120. 三角形最小路径和
  *
+ *  62. 不同路径
+ *
  */
 public class DynamicPlanning {
 
@@ -372,6 +374,37 @@ public class DynamicPlanning {
         }else{
             return triangle.get(0).get(0);
         }
+    }
+
+    /**
+     * 62. 不同路径
+     * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+     * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。问总共有多少条不同的路径？
+     * 输入: m = 3, n = 2     输出: 3
+     * 解释:从左上角开始，总共有 3 条路径可以到达右下角。
+     * 1. 向右 -> 向右 -> 向下      2. 向右 -> 向下 -> 向右     3. 向下 -> 向右 -> 向右
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        if(m == 0 || n == 0){
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    continue;
+                }else if(i == 0 || j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String[] args){
