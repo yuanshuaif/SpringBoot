628d432e582e4a87590e603bb17426dc17dd634b
yuanshuaif <yuanshuaif@yonyou.com>
2020-09-26 10:38:48 +0800

    小程序(197):找到所有数组中消失的数字

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -42,6 +42,8 @@ import java.util.*;
  * 题目442:数组中重复的数据
  *
  * 题目119:杨辉三角 II
+ *
+ * 题目448:找到所有数组中消失的数字
  */
 public class Array {
 
@@ -688,6 +690,31 @@ public class Array {
 
             }
             pre = res;
+        }
+        return res;
+    }
+
+    /**
+     * 448. 找到所有数组中消失的数字
+     * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+     * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+     * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+     * 输入:[4,3,2,7,8,2,3,1]     输出:[5,6]
+     * @param nums
+     * @return
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0){
+                nums[index] = -nums[index];
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                res.add(i + 1);
+            }
         }
         return res;
     }
