594c15689ccbe0675efeab464dd314780bd50480
yuanshuaif <yuanshuaif@yonyou.com>
2020-10-02 00:09:40 +0800

    小程序(210):宝石与石头

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Strings2.java
@@ -24,6 +24,8 @@ import java.util.Set;
  *
  * 657. 机器人能否返回原点
  *
+ * 771. 宝石与石头
+ *
  * Created by 10326 on 2020/8/26.
  */
 public class Strings2 {
@@ -324,6 +326,37 @@ public class Strings2 {
             }
         }
         return x == 0 && y == 0;
+    }
+
+    /**
+     * 771. 宝石与石头
+     *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+     *   J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+     *   输入: J = "aA", S = "aAAbbbb"    输出: 3           输入: J = "z", S = "ZZ"       输出: 0
+     * @param J
+     * @param S
+     * @return
+     */
+    public int numJewelsInStones(String J, String S) {
+        // int res = 0;
+        // for(int i = 0; i < S.length(); i++){
+        //     if(J.contains(S.charAt(i) + "")){
+        //         res++;
+        //     }
+        // }
+        // return res;
+
+        int res = 0;
+        Set<Character> hash = new HashSet<>();
+        for(int i = 0; i < J.length(); i++){
+            hash.add(J.charAt(i));
+        }
+        for(int i = 0; i < S.length(); i++){
+            if(hash.contains(S.charAt(i))){
+                res++;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args){
