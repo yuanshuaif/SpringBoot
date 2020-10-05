175b4c224a095a236aa999e9bde7af2ddbb2d27e
yuanshuaif <yuanshuaif@yonyou.com>
2020-10-05 18:02:59 +0800

    小程序(220):四数之和

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -46,6 +46,8 @@ import java.util.*;
  * 题目448:找到所有数组中消失的数字
  *
  * 题目66:加一
+ *
+ * 题目18:四数之和
  */
 public class Array {
 
@@ -776,6 +778,68 @@ public class Array {
             }
         }
         return digits;
+    }
+
+    /**
+     * 18. 四数之和
+     * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
+     * 注意：答案中不可以包含重复的四元组。
+     * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。满足要求的四元组集合为：[[-1,  0, 0, 1],[-2, -1, 1, 2],[-2,  0, 0, 2]]
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(nums == null || nums.length < 4){
+            return ans;
+        }
+        Arrays.sort(nums);
+        int len = nums.length;
+        // 不能重复：1.当前层的当前元素不能与同层的前一个元素相同；2.1.当前层的当前元素不能小于上一层的元素；
+        for(int i = 0; i < len - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            if(nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target){//最小大于target
+                break;
+            }
+            if(nums[i] + nums[len - 1] + nums[len - 2] + nums[len - 3] < target){
+                continue;
+            }
+            for(int j = i + 1; j < len - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                if(nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target){//最小大于target
+                    break;
+                }
+                if(nums[i] + nums[j] + nums[len - 1] + nums[len - 2] < target){
+                    continue;
+                }
+                int left = j + 1;
+                int right = len - 1;
+                while(left < right){
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum < target){
+                        left++;
+                    }else if(sum > target){
+                        right--;
+                    }else{
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        }
+                        left++;
+                        while(left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args){
