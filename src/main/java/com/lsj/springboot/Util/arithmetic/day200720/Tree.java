9cf22b1ee579a01c80fdf8a3c7a5777fa860624f
yuanshuaif <yuanshuaif@yonyou.com>
2020-10-05 10:57:57 +0800

    小程序(217):BiNode

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200720/Tree.java
@@ -89,6 +89,8 @@ import java.util.LinkedList;
  *  剑指 Offer 68 - II. 二叉树的最近公共祖先
  *
  *  面试题 04.05. 合法二叉搜索树
+ *
+ *  面试题 17.12. BiNode
  */
 public class Tree {
 
@@ -1205,6 +1207,35 @@ public class Tree {
             return false;
         }
         return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+    /**
+     * 面试题 17.12. BiNode
+     * 二叉树数据结构TreeNode可用来表示单向链表（其中left置空，right为下一个链表节点）。
+     * 实现一个方法，把二叉搜索树转换为单向链表，要求依然符合二叉搜索树的性质，转换操作应是原址的，也就是在原始的二叉搜索树上直接修改。
+     * 返回转换后的单向链表的头节点。
+     * [5,2,7,1,4,6,8,0,null,3]             [0,null,1,null,2,null,3,null,4,null,5,null,6,null,7,null,8]
+     */
+    TreeNode head = new TreeNode(-1);
+    TreeNode pre = null;
+    public TreeNode convertBiNode(TreeNode root) {
+        helper(root);
+        return head.right;
+    }
+    public void helper(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        helper(root.left);
+        if(pre == null){
+            pre = root;
+            head.right = root;
+        }else{
+            pre.right = root;
+            pre = pre.right;
+        }
+        root.left = null;
+        helper(root.right);
     }
 
     public static void main(String[] args){
