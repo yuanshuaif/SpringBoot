bf56b7af09dfdb6241c6e916b8b678edf6df3cee
yuanshuaif <yuanshuaif@yonyou.com>
2020-10-07 17:44:24 +0800

    小程序(225):重塑矩阵

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200806/Array.java
@@ -55,6 +55,8 @@ import java.util.*;
  * 剑指 Offer 29. 顺时针打印矩阵
  *
  * 题目867:转置矩阵
+ *
+ * 题目566:重塑矩阵
  */
 public class Array {
 
@@ -944,6 +946,35 @@ public class Array {
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 ans[j][i] = A[i][j];
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 566. 重塑矩阵
+     * 在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+     * 给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+     * 如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+     * 输入:nums =[[1,2],[3,4]]r = 1, c = 4       输出:[[1,2,3,4]]       解释:行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+     * 输入:nums =[[1,2],[3,4]]r = 2, c = 4       输出:[[1,2],[3,4]]     解释:没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+     * @param nums
+     * @param r
+     * @param c
+     * @return
+     */
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int row = nums.length;
+        int col = nums[0].length;
+        if(row * col != r * c || col == c){
+            return nums;
+        }
+        int count = 0;
+        int[][] ans = new int[r][c];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                ans[count / c][count % c] = nums[i][j];
+                count++;
             }
         }
         return ans;
