599fac140303b9ed060d4b6fbd523c9e2ff779b1
袁帅 <yuanshuaif@yonyou.com>
2020-10-10 12:55:42 +0800

    小程序(231):除自身以外数组的乘积

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
@@ -55,6 +55,8 @@ import java.util.Stack;
  * 845. 数组中的最长山脉
  *
  * 718. 最长重复子数组
+ *
+ * 238. 除自身以外数组的乘积
  */
 public class DynamicPlanning {
 
@@ -815,6 +817,34 @@ public class DynamicPlanning {
         return ans;
     }
 
+
+    /**
+     * 238. 除自身以外数组的乘积
+     * 给你一个长度为 n 的整数数组 nums，其中 n > 1，返回输出数组 output ，其中 output[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积。
+     * 输入: [1,2,3,4]        输出: [24,12,8,6]
+     * 提示：题目数据保证数组之中任意元素的全部前缀元素和后缀（甚至是整个数组）的乘积都在 32 位整数范围内。
+     * 说明: 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+     * 进阶：你可以在常数空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+     * @param nums
+     * @return
+     */
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        left[0] = right[n - 1] = 1;
+        for(int i = 1; i < n; i++){
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--){
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        int[] ans = new int[n];
+        for(int i = 0; i < n; i++){
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
     public static void main(String[] args){
       /*  int[][] costs = new int[3][3];
         costs[0][0] = 17;
