b395906e5c6f243124ea262a167c3c9c41089bb7
袁帅 <yuanshuaif@yonyou.com>
2020-10-10 13:39:41 +0800

    小程序(231):除自身以外数组的乘积

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200809/DynamicPlanning.java
@@ -831,17 +831,15 @@ public class DynamicPlanning {
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = right[n - 1] = 1;
+        int[] ans = new int[n];
+        left[0] = 1;
         for(int i = 1; i < n; i++){
             left[i] = left[i - 1] * nums[i - 1];
         }
-        for(int i = n - 2; i >= 0; i--){
-            right[i] = right[i + 1] * nums[i + 1];
-        }
-        int[] ans = new int[n];
-        for(int i = 0; i < n; i++){
-            ans[i] = left[i] * right[i];
+        int rightAns = 1;
+        for(int i = n - 1; i >= 0; i--){
+            ans[i] = left[i] * rightAns;
+            rightAns *= nums[i];
         }
         return ans;
     }
@@ -866,6 +864,7 @@ public class DynamicPlanning {
         List<List<Integer>> params = new ArrayList(){{add(param1);add(param2);add(param3);add(param4);}};
         System.out.println(minimumTotal(params));*/
 //        System.out.println(lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
-        System.out.println(findLength(new int[]{1,2,3,2,1}, new int[]{3,2,1,4,7}));
+       /* System.out.println(findLength(new int[]{1,2,3,2,1}, new int[]{3,2,1,4,7}));*/
+        System.out.println(productExceptSelf(new int[]{1,2,3,4}));
     }
 }
