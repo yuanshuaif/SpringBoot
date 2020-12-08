814080d956654a8fb3a3288ffdf0e7f575c06f4e
袁帅 <yuanshuaif@yonyou.com>
2020-12-08 20:14:27 +0800

    排序算法——交换排序

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/SortAlgorithm2.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/SortAlgorithm2.java
@@ -6,7 +6,7 @@ package com.lsj.springboot.Util.arithmetic.day200812;
  * https://mp.weixin.qq.com/s/_B9y97J1vM8dhdQJNq_F9w
  *
  * https://mp.weixin.qq.com/s/ZygPOjMMcwtOgLSVVzJlIw
- * 插入排序：直接插入排序和希尔排序两种
+ * 插入排序：直接插入排序和希尔排序两种(将数组分为有序、无序数组，依次将无序数组中的元素插入到有序数组中)
  *
  * https://mp.weixin.qq.com/s/QG5gmGHpqzNrkltVwbb0yQ
  * 交换排序：冒泡排序和快速排序（如果发现两个记录的次序相反时即进行交换，直到没有反序位置）
@@ -16,8 +16,8 @@ package com.lsj.springboot.Util.arithmetic.day200812;
 public class SortAlgorithm2 {
 
     public static void main(String[] args){
-        int[] array = new int[]{9, 3, 8, 7, 4, 5, 1, 2, 10};
-        sort2(array);
+        int[] array = new int[]{9, 3, 8, 7, 5, 9, 1, 2, 10, 9};
+        sort4(array);
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
@@ -78,7 +78,60 @@ public class SortAlgorithm2 {
      * @param arrays
      */
     private static void sort3(int[] arrays){
+        int temp;
+        for(int i = 1; i < arrays.length; i++){// 控制循环次数
+            for(int j = 0; j < arrays.length - i; j++){
+                if(arrays[j] > arrays[j + 1]){
+                    temp = arrays[j];
+                    arrays[j] = arrays[j + 1];
+                    arrays[j + 1] = temp;
+                }
+            }
+        }
+    }
 
+    /**
+     * 4.快速排序
+     * 是冒泡排序的改进，比较和交换从两端向中间进行。
+     * 过程：
+     * 1.设两个指针l和r，初值分别为low和high，基准为x=R[l]，
+     * 2.首先从r位置开始向前搜索第一个小于基准x.key的记录存入l所指位置上，l自增1，
+     * 3.然后从l所指位置向后搜索找到第一个大于基准x.key的记录存入r所指位置上，r自减1，
+     * 4.重复直至l=r为止。
+     * 5.将基准值付给R[l]或者R[r]
+     * 6.左右进行递归
+     * 时间复杂度：O(nlog2n)  空间复杂度：O(log2n)  稳定性：不稳定
+     * @param arrays
+     */
+    private static void sort4(int[] arrays){
+        sort4(arrays, 0, arrays.length - 1);
+    }
+
+    private static void sort4(int[] arrays, int left, int right){
+        int l = left;
+        int r = right;
+        int x = arrays[l];
+        while(l < r){
+            while(l < r && arrays[r] > x){
+                r--;
+            }
+            if(l < r){
+                arrays[l++] = arrays[r];
+            }
+            while(l < r && arrays[l] < x){
+                l++;
+            }
+            if(l < r){
+                arrays[r--] = arrays[l];
+            }
+        }
+        arrays[r] = x;// l == r 上述循环终止条件
+        if(left < l){
+            sort4(arrays, left, l - 1);
+        }
+        if(r < right){
+            sort4(arrays, r + 1, right);
+        }
     }
 
 }
