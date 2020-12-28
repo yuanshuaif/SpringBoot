f41cd88aa416005111d48011ae2e05ec92fb0c31
袁帅 <yuanshuaif@yonyou.com>
2020-12-28 20:26:00 +0800

    hash算法

--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/hash/Hash.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/hash/Hash.java
@@ -1,4 +1,4 @@
-package com.lsj.springboot.Util.arithmetic.day200812;
+package com.lsj.springboot.Util.arithmetic.day200812.hash;
 
 import com.lsj.springboot.Util.arithmetic.day200818.Maths;
 
--- src/main/java/com/lsj/springboot/Util/arithmetic/day200812/hash/HashTable.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/day200812/hash/HashTable.java
@@ -1,4 +1,4 @@
-package com.lsj.springboot.Util.arithmetic.day200812.search;
+package com.lsj.springboot.Util.arithmetic.day200812.hash;
 
 public class HashTable<K, V> {
 
