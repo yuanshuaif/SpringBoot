f1d880f88e6b99eaab33203fb12e33273a8d15c0
袁帅 <yuanshuaif@yonyou.com>
2021-01-19 17:25:08 +0800

    回溯算法

--- src/main/java/com/lsj/springboot/Util/arithmetic/hasDo/backtracking/Backtracking.java
+++ src/main/java/com/lsj/springboot/Util/arithmetic/hasDo/backtracking/Backtracking.java
@@ -1,11 +1,14 @@
-package com.lsj.springboot.Util.arithmetic.toDo;
+package com.lsj.springboot.Util.arithmetic.hasDo.backtracking;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
+ *
  * 回溯算法
+ * (求所有解的算法)
+ *
  * 78. 子集
  *
  * 90. 子集 II
@@ -16,14 +19,15 @@ import java.util.List;
  *
  * 47. 全排列 II
  *
- * 77. 组合
- *
  * 面试题 08.07. 无重复字符串的排列组合
  *
  * 面试题 08.08. 有重复字符串的排列组合II
  * 剑指 Offer 38. 字符串的排列
  *
  * 22. 括号生成
+ *
+ * 77. 组合
+ *
  */
 public class Backtracking {
 
@@ -79,7 +83,7 @@ public class Backtracking {
     /**
      * 90. 子集 II
      * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。说明：解集不能包含重复的子集。
-     * 输入: [1,2,2]  输出: [[2], [1],[1,2,2],[2,2], [1,2],[]]
+     * 输入: [1,2,2]  输出: [[][1][1,2][1,2,2]  [2][2,2]]
      * @param nums
      * @return
      */
@@ -94,7 +98,7 @@ public class Backtracking {
         ans.add(new ArrayList<>(temp));
         // 第一层4种选择；第二层3种选择；第三层2种选择；第四层1种选择
         for(int j = k; j < nums.length; j++){
-            if(j > k && nums[j] == nums[j - 1]){// 与上一个值相等，不作为本层的选择
+            if(j > k && nums[j] == nums[j - 1]){// 本层内已经选择过，则不再考虑
                 continue;
             }
             temp.add(nums[j]);// 本层做出选择
@@ -105,11 +109,9 @@ public class Backtracking {
 
     /**
      * 39. 组合总和
-     * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
-     * candidates 中的数字可以无限制重复被选取。
+     * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。candidates 中的数字可以无限制重复被选取。
      * 所有数字（包括 target）都是正整数。 解集不能包含重复的组合。
-     * 输入：candidates = [2,3,6,7], target = 7,所求解集为：[[7],[2,2,3]]
-     * 输入：candidates = [2,3,5], target = 8,所求解集为：[[2,2,2,2],[2,3,3],[3,5]]
+     * 输入：candidates = [2,3,6,7], target = 7,所求解集为：[[7],[2,2,3]];   输入：candidates = [2,3,5], target = 8,所求解集为：[[2,2,2,2],[2,3,3],[3,5]]
      * 1 <= candidates.length <= 30         1 <= candidates[i] <= 200       candidate 中的每个元素都是独一无二的。        1 <= target <= 500
      * @param candidates
      * @param target
@@ -186,70 +188,33 @@ public class Backtracking {
         }
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        permuteUnique(nums, res, new ArrayList<>(), new ArrayList<>());
+        permuteUnique(nums, res, new ArrayList<>(), new boolean[nums.length]);
         return res;
     }
 
-    public static void permuteUnique(int[] nums, List<List<Integer>> res, List<Integer> temp, List<Integer> index){
+    public static void permuteUnique(int[] nums, List<List<Integer>> res, List<Integer> temp, boolean[] used){
         if(temp.size() == nums.length){
             res.add(new ArrayList<>(temp));
             return;
         }
         for(int i = 0; i < nums.length; i++){
-            if(index.contains(i)){
-                continue;
-            }
-            // 剪枝的核心算法：1）值与前一个值相等；2）前面一个没用到剪掉（说明前一个在前面的分支里已经用过了）
-            if(i > 0 && nums[i] == nums[i - 1] && !index.contains(i - 1)){
+            // 剪枝的核心算法：1)已经使用过的元素剪掉 2）某一个元素与前一个的元素是否相等，相等且上层没有使用前一个元素，说明前一个元素在本层使用了，剪掉当前元素
+            // （上层没有使用过的本层重复元素需要剪掉）
+            if(used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])){
                 continue;
             }
             temp.add(nums[i]);
-            index.add(i);
-            permuteUnique(nums, res, temp, index);
+            used[i] = true;
+            permuteUnique(nums, res, temp, used);
             temp.remove(temp.size() - 1);
-            index.remove(index.size() - 1);
+            used[i] = false;
         }
-    }
-
-    /**
-     * 77. 组合
-     * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
-     * 输入: n = 4, k = 2     输出: [[2,4],[3,4],[2,3],[1,2],[1,3],[1,4]]
-     * @param n
-     * @param k
-     * @return
-     */
-    public List<List<Integer>> combine(int n, int k) {
-        if(k > n){
-            return new ArrayList<>();
-        }
-        int[] nums = new int[n];
-        for(int i = 1; i <= n; i++){
-            nums[i - 1] = i;
-        }
-        List<List<Integer>> res = new ArrayList<>();
-        combine(nums, res, new ArrayList<>(), 0, k);
-        return res;
-    }
-
-    public void combine(int[] nums, List<List<Integer>> res, List<Integer> temp, int start, int k) {
-        if(temp.size() == k){
-            res.add(new ArrayList<>(temp));
-            return;
-        }
-        for(int i = start; i < nums.length; i++){
-            temp.add(nums[i]);
-            combine(nums, res, temp, i + 1, k);
-            temp.remove(temp.size() - 1);
-        }
-        return;
     }
 
     /**
      * 面试题 08.07. 无重复字符串的排列组合
      * 无重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合，字符串每个字符均不相同。
-     * 输入：S = "qwe"     输出：["qwe", "qew", "wqe", "weq", "ewq", "eqw"]
-     * 输入：S = "ab"      输出：["ab", "ba"]
+     * 输入：S = "qwe"     输出：["qwe", "qew", "wqe", "weq", "ewq", "eqw"]；      输入：S = "ab"      输出：["ab", "ba"]
      * 1.字符都是英文字母。2.字符串长度在[1, 9]之间。
      * @param S
      * @return
@@ -297,7 +262,7 @@ public class Backtracking {
         for(int i = 0; i < ch.length; i++){
             if(!used[i]){
                 used[i] = true;
-                permutation(ch, ans, cur, used);
+                permutation(ch, ans, cur + ch[i], used);
                 used[i] = false;
             }
         }
@@ -311,7 +276,7 @@ public class Backtracking {
      * @param S
      * @return
      */
-    public String[] permutation2(String S) {
+    public static String[] permutation2(String S) {
         List<String> ans = new ArrayList<>();
         char[] ch = S.toCharArray();
         Arrays.sort(ch);
@@ -319,21 +284,19 @@ public class Backtracking {
         return ans.toArray(new String[0]);
     }
 
-    public void permutation2(char[] ch, List<String> ans, String cur, boolean[] used) {
+    public static void permutation2(char[] ch, List<String> ans, String cur, boolean[] used) {
         if(cur.length() == ch.length){
             ans.add(cur);
             return;
         }
         for(int i = 0; i < ch.length; i++){
-            // 同一层与前一个元素相等；且上一层没有被使用，说明在其他分支已用
-            if(i > 0 && ch[i] == ch[i - 1] && !used[i - 1]){
+            // 1）已使用过；2）同一层与前一个元素相等且上层没有被使用，说明在本层已用，剪掉
+            if(used[i] || (i > 0 && ch[i] == ch[i - 1] && !used[i - 1])){
                 continue;
             }
-            if(!used[i]){
-                used[i] = true;
-                permutation2(ch, ans, cur + ch[i], used);
-                used[i] = false;
-            }
+            used[i] = true;
+            permutation2(ch, ans, cur + ch[i], used);
+            used[i] = false;
         }
     }
 
@@ -367,11 +330,45 @@ public class Backtracking {
         }
     }
 
+    /**
+     * 77. 组合
+     * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+     * 输入: n = 4, k = 2     输出: [[1,2],[1,3],[1,4], [2,3],[2,4], [3,4]]
+     * @param n
+     * @param k
+     * @return
+     */
+    public List<List<Integer>> combine(int n, int k) {
+        if(k > n){
+            return new ArrayList<>();
+        }
+        int[] nums = new int[n];
+        for(int i = 1; i <= n; i++){
+            nums[i - 1] = i;
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        combine(nums, res, new ArrayList<>(), 0, k);
+        return res;
+    }
+
+    public void combine(int[] nums, List<List<Integer>> res, List<Integer> temp, int start, int k) {
+        if(temp.size() == k){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for(int i = start; i < nums.length; i++){
+            temp.add(nums[i]);
+            combine(nums, res, temp, i + 1, k);
+            temp.remove(temp.size() - 1);
+        }
+        return;
+    }
+
     public static void main(String[] args){
 //        System.out.println(subsets(new int[]{1,2,3}));
 //        System.out.println(permute(new int[]{1,2,3}));
 //        System.out.println(permuteUnique(new int[]{1,1,2}));
-        System.out.println(permutation("qwe"));
+        System.out.println(permutation2("qqe"));
     }
 
 
