b5e2301aec92703770545c3c8ad27bfa1963e9bc
袁帅 <yuanshuaif@yonyou.com>
2021-01-22 11:05:50 +0800

    rename

--- src/main/java/com/lsj/springboot/entity/UserAnnotation.java
+++ src/main/java/com/lsj/springboot/entity/UserAnnotation.java
@@ -11,13 +11,37 @@ import java.io.Serializable;
  * Created by 10326 on 2019/4/13.
  * lombok的使用
  */
-@Getter
+/*@Getter
 @Setter
-@ToString
+@ToString*/
 public class UserAnnotation implements Serializable{
     @NotNull
     private String name;
     private int age;
     @NotNull
     private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
