33cc181466b84bf22e34b1e8ffb0309fa7bc4cdc
yuanshuaif <yuanshuaif@yonyou.com>
2021-08-06 11:22:33 +0800

    @Conditional的执行时机

--- src/main/java/com/lsj/springboot/config/DemoConfig.java
+++ src/main/java/com/lsj/springboot/config/DemoConfig.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(Demo.class)
 // 使用指定的配置文件，留给开发者使用的：application.properties或者application.yml
 //
-@ConditionalOnProperty(prefix = "spring.application", name = "name", havingValue = "fibz-bx")
+//@ConditionalOnProperty(prefix = "spring.application", name = "name", havingValue = "fibz-bx")
+@Conditional(MyCondition.class)
 public class DemoConfig {
 
     @Autowired
