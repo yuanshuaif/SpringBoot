28693643fa14cdc67a43f5c6ab9b6647331b70b5
yuanshuaif <yuanshuaif@yonyou.com>
2021-08-06 23:05:30 +0800

    ehcache 作为本地缓存

--- src/main/java/com/lsj/springboot/ehcache/EhcacheConfiguration.java
+++ src/main/java/com/lsj/springboot/ehcache/EhcacheConfiguration.java
@@ -3,18 +3,19 @@ package com.lsj.springboot.ehcache;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-/*import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;*/
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.core.io.ClassPathResource;
 
 
 /**
  * Created by 10326 on 2020/12/6.
  */
 @Configuration
-@EnableCaching
+//@EnableCaching
 public class EhcacheConfiguration {
 
-    /*@Bean
+/*    @Bean
     public EhCacheManagerFactoryBean cacheManagerFactoryBean(){
         EhCacheManagerFactoryBean bean = new EhCacheManagerFactoryBean();
         bean.setConfigLocation(new ClassPathResource("ehcache.xml"));
--- src/main/java/com/lsj/springboot/rabbitMQ/deadletter/ConsumersDemo5Deal.java
+++ src/main/java/com/lsj/springboot/rabbitMQ/deadletter/ConsumersDemo5Deal.java
@@ -1,20 +1,16 @@
 package com.lsj.springboot.rabbitMQ.deadletter;
 
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
-
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.stereotype.Component;
-import com.alibaba.fastjson.JSONObject;
-import com.rabbitmq.client.Channel;
-
-import lombok.extern.slf4j.Slf4j;
 
 
 /**
