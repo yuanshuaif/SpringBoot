624f378e1ba8c50421e45ad078bc5158fe40fc01
yuanshuaif <yuanshuaif@yonyou.com>
2022-05-02 18:12:55 +0800

    redis分布式session

---
+++ src/main/java/com/lsj/springboot/redis/redisSession/RedisSessionConfig.java
@@ -0,0 +1,12 @@
+package com.lsj.springboot.redis.redisSession;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by Administrator on 2019/3/9.
+ */
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+}
