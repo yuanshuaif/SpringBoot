847efaaa284948126b5d5710be1df412742348cd
yuanshuaif <yuanshuaif@yonyou.com>
2022-05-24 22:14:12 +0800

    小程序(238):合并K个升序链表

--- src/main/java/com/lsj/springboot/arithmetic/listNode/LinkedList.java
+++ src/main/java/com/lsj/springboot/arithmetic/listNode/LinkedList.java
@@ -70,6 +70,8 @@ import java.util.*;
  * 92. 反转链表 II
  *
  * 1669. 合并两个链表
+ *
+ * 23. 合并K个升序链表
  */
 public class LinkedList {
 
@@ -1121,6 +1123,60 @@ public class LinkedList {
         tail2.next = list1.next;
         list1.next = head2;
         return head1;
+    }
+
+    /**
+     * 23. 合并K个升序链表
+     * 给你一个链表数组，每个链表都已经按升序排列。请你将所有链表合并到一个升序链表中，返回合并后的链表。
+     * 输入：lists = [[1,4,5],[1,3,4],[2,6]]   输出：[1,1,2,3,4,4,5,6]
+     * 输入：lists = []    输出：[]   输入：lists = [[]] 输出：[]
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0){
+            return null;
+        } else if(lists.length == 1){
+            return lists[0];
+        }
+        int mid = lists.length / 2;
+        ListNode left = mergeKLists(Arrays.copyOf(lists, mid));
+        ListNode right = mergeKLists(Arrays.copyOfRange(lists, mid, lists.length));
+        return mergeLists(left, right);
+    }
+    public ListNode mergeLists(ListNode left, ListNode right) {
+        if(left == null){
+            return right;
+        }else if(right == null){
+            return left;
+        }
+        ListNode ans = new ListNode(0);
+        ListNode cur = ans;
+        while(left != null && right != null){
+            if(left.val < right.val){
+                cur.next = left;
+                left = left.next;
+                cur = cur.next;
+            }else if(right.val < left.val){
+                cur.next = right;
+                right = right.next;
+                cur = cur.next;
+            }else{
+                cur.next = left;
+                left = left.next;
+                cur = cur.next;
+                cur.next = right;
+                right = right.next;
+                cur = cur.next;
+            }
+        }
+        if(left != null){
+            cur.next = left;
+        }
+        if(right != null){
+            cur.next = right;
+        }
+        return ans.next;
     }
 
     public static void main(String[] args){
