082861e06f8a3cbf53bf461a3f0d836decf2c2e9
yuanshuaif <yuanshuaif@yonyou.com>
2022-07-17 13:29:46 +0800

    数组

--- src/main/java/com/lsj/springboot/arithmetic/array/DoublePointer.java
+++ src/main/java/com/lsj/springboot/arithmetic/array/DoublePointer.java
@@ -77,7 +77,7 @@ public class DoublePointer {
      * 88. 合并两个有序数组(10.01)
      * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
      * 说明:初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
-     * 输入: nums1 = [1,2,3,0,0,0], m = 3   nums2 = [2,5,6],       n = 3    输出: [1,2,2,3,5,6]
+     * 输入: nums1 = [1,2,3,6,0,0,0], m = 4   nums2 = [2,5,6],       n = 3    输出: [1,2,2,3,5,6,6]
      * @param nums1
      * @param m
      * @param nums2
@@ -123,19 +123,25 @@ public class DoublePointer {
             return firstList;
         }
         List<Integer> combine = new ArrayList<>();
-        for(int i = 0, j = 0; i < firstList.length && j < secondList.length;){
+        int i = 0, j = 0;
+        for(; i < firstList.length && j < secondList.length;){
             if(firstList[i] == secondList[j]){
-                combine.add(firstList[i]);
-                i++;j++;
+                combine.add(firstList[i++]);
+                combine.add(secondList[i++]);
+            }else if(firstList[i] < secondList[j]){
+                combine.add(firstList[i++]);
             }else {
-                int max = Math.max(firstList[i], secondList[j]);
-                if(firstList[i] == max){
-                    combine.add(secondList[j]);
-                    j++;
-                }else if(secondList[j] == max){
-                    combine.add(firstList[i]);
-                    i++;
-                }
+                combine.add(secondList[j++]);
+            }
+        }
+        if(i < firstList.length){
+            while (i < firstList.length){
+                combine.add(firstList[i++]);
+            }
+        }
+        if(j < firstList.length){
+            while (j < firstList.length){
+                combine.add(firstList[j++]);
             }
         }
         return combine.toArray(new Integer[combine.size()]);
@@ -263,6 +269,7 @@ public class DoublePointer {
     /**
      * 345. 反转字符串中的元音字母（类似于125）
      * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。    输入: "leetcode"    输出: "leotcede"
+     * 元音字母：aeiouAEIOU
      * @param s
      * @return
      */
@@ -287,7 +294,6 @@ public class DoublePointer {
         }
         return new String(chars);
     }
-
 
     /**
      * 977. 有序数组的平方
